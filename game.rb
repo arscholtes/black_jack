070a58a179b1628e3484ef6353eff5326e498d1f
Alex <alex@ascholtes.me>
2017-07-23 14:16:22 -0400

    I love ruby

--- game.rb
+++ game.rb
@@ -22,6 +22,7 @@ class Games
     if gets.chomp.downcase == "y"
       2.times {@player_hand << @deck.draw}
       2.times {@dealer_hand << @deck.draw}
+      calculate
       render_hand
       game_loop
     else
@@ -29,11 +30,16 @@ class Games
     end
   end
 
-  def game_loop
-    hit?
-    @player_sum = @dealer_sum = 0
+  def calculate
+    @player_sum = 0
+    @dealer_sum = 0
     @player_hand.each{|card| @player_sum += card.value}
     @dealer_hand.each{|card| @dealer_sum += card.value}
+  end
+
+  def game_loop
+    hit?
+    calculate
     if @player_sum >= 21 or @dealer_sum >= 21
       end_game
     else
@@ -58,6 +64,13 @@ class Games
   end
 
   def end_game
+    while @dealer_sum < 17
+      card = @deck.draw
+      @dealer_hand << card
+      @dealer_sum += card.value
+      puts("Dealer: #{@dealer_hand.each{|card| card.to_s}.join ", "}")
+    end
+
     if @player_sum > 21
       puts "player busts"
     elsif @dealer_sum > 21
